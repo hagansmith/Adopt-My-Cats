a5fa809c8f6c63e5848aed169f1fedeec4a5d030
Adam Smith <hagansmith@gmail.com>
2017-09-27 19:21:34 -0500

    add files and intial instructions and comments

---
+++ data/cats.js
@@ -0,0 +1,4 @@
+"use strict";
+
+//get data from source
+//https://random-dogs-api.herokuapp.com/cats/NUMBEROFCATZZZZZ (obviously replacing NUMBEROFCATZZZZZ with the actual number I entered in step 1).
---
+++ javascripts/events.js
@@ -0,0 +1,10 @@
+"use strict";
+
+//submit button
+
+
+//dom function
+
+//hide input
+
+//new button
